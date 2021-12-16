eec9d2cd8c0f3670ea6213167f96ea447558b67e
Juan Calvo Hoyos <jdcalvo@utp.edu.co>
2021-12-15 20:04:38 -0500

    url backheroku

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ import IndexInscripciones from 'pages/inscripciones';
 // import PrivateRoute from 'components/PrivateRoute';
 
 const httpLink = createHttpLink({
-  uri: 'http://localhost:4000/graphql',
+  uri: 'https://back-botsitos.herokuapp.com/graphql',
 });
 
 const authLink = setContext((_, { headers }) => {
